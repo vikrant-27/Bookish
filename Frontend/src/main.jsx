521b1cbec197f351b7dc704441475d289c4b10fa
vikrant-27 <vikrantsarade2002@gmail.com>
2024-07-22 22:16:40 +0530

    revert

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -1,14 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { HashRouter } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 import App from './App.jsx'
 import './index.css'
 
-ReactDOM.render(
-  <React.StrictMode>
-    <HashRouter>
-      <App />
-    </HashRouter>
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <BrowserRouter>
+
+    <App />
+  
+  </BrowserRouter>
+)
--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base:'/',   //getting blank screen while deployment so removed 
+  // base:'/Bookish/',   //getting blank screen while deployment so removed 
   plugins: [react()],
 })
