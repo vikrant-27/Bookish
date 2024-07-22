2bc0216e7f21b317a8d5c96ea887ea2ea8084357
vikrant-27 <vikrantsarade2002@gmail.com>
2024-07-22 22:03:11 +0530

    hashrouter

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -1,13 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import App from './App.jsx'
 import './index.css'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <BrowserRouter>
-
-    <App />
-  
-  </BrowserRouter>
-)
+ReactDOM.render(
+  <React.StrictMode>
+    <HashRouter>
+      <App />
+    </HashRouter>
+  </React.StrictMode>,
+  document.getElementById('root')
+);
