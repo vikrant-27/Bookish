f180aed8a5a577fcc33581c47989aa844fa20c97
vikrant-27 <vikrantsarade2002@gmail.com>
2024-07-28 11:25:35 +0530

    connection established

--- Backend/index.js
+++ Backend/index.js
@@ -4,15 +4,30 @@ import mongoose from 'mongoose';
 
 const app = express();
 dotenv.config();
-const PORT = process.env.PORT || 4000;
-const MONGO_URI = process.env.MONGO_URI || 'mongodb://localhost:27017/';
 
-mongoose.connect(MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => console.log('Connected to MongoDB'))
-  .catch((error) => console.error(error));
+
+
+const PORT = process.env.PORT || 4000;
+const MONGO_URI = process.env.MONGO_URI;
+console.log(PORT)
+
+// try{
+//   mongoose.connect(MONGO_URI,{
+//   });
+//   console.log("connected to mongoDB")     try catch simple mtd
+
+// }
+// catch(error){
+// console.log("Error ",error )
+// }
+
+mongoose.connect(MONGO_URI, {
+}).then(() => {
+  console.log("Connected to MongoDB");
+}).catch((error) => {
+  console.log("Error connecting to MongoDB:", error);
+});
 
 app.listen(PORT, () => {
-  console.log(`Example app listening on port ${PORT}`);
-}, (error) => {
-  console.error('Error starting server:', error);
-});
+  console.log(`Bookish app listening on port ${PORT}`);
+},);
